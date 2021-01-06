eb3a93313818955f4a326f2b60eefd9df475369e
alexivison <alexivison@users.noreply.github.com>
2021-01-06 03:42:47 +0000

    deploy: 9a3a59794f814caedc60ffc6c87bc85abe5c0d9c

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Kaipuu/precache-manifest.6495fd7c98dfde61fd88185e26f90548.js"
+  "/precache-manifest.90230d7147ebd5cb2637dd073bc16ac4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Kaipuu/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
